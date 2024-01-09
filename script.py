86e53667e9a9128651afdfe38f3d8f34ea3a5982
juanobyte <85565647+juanobyte@users.noreply.github.com>
2024-01-09 10:20:15 -0600

    script.py

--- script.py
+++ script.py
@@ -22,25 +22,29 @@ import easygui
 
 # Reemplaza 'nombre_del_archivo.xlsx' con el nombre de tu archivo Excel
 excel_file = pd.ExcelFile('PRIME.xlsx')
-opx_file = pd.ExcelFile('opx_new.xlsx')
-#easygui.msgbox("The file was processed successfully")
+opx_file = pd.ExcelFile('opx_new_1.xlsx')
+easygui.msgbox("The file was processed successfully")
 
+anio =  int(easygui.enterbox("Type the year"))
+str_anio = str(anio)
 
-#numhojas = int(easygui.enterbox("Input the num of the sheets do you want to work"))
-numhojas = 1
+numhojas = 0
+
+numhojas = int(easygui.enterbox("Input the number of the prime sheets (or months) do you want to work on"))
+#numhojas = 1
 hojas = []
 hojas_2 = []
-#for i in range(numhojas):
- #   hojas.append(str(easygui.enterbox("In which sheet do you want to work? sheet number "  + str(i + 1))))
+for i in range(numhojas):
+   hojas.append(str(easygui.enterbox("In which prime file sheet (or month) do you want to work? sheet number:"  + str(i + 1))))
 
-#PRIME
-hojas.append("Associate_Jan")
-hojas.append("Associate_Feb")
-hojas.append("Associate_March")
-hojas.append("Associate_April")
-hojas.append("Associate_May")
-hojas.append("Associate_June")
-hojas.append("Associate_July")
+#PRIME#
+#hojas.append("Associate_Jan")
+#hojas.append("Associate_Feb")
+#hojas.append("Associate_March")
+#hojas.append("Associate_April")
+#hojas.append("Associate_May")
+#hojas.append("Associate_June")
+#hojas.append("Associate_July")
 
 #OPX
 hojas_2.append("Table")
@@ -67,48 +71,55 @@ palabra = "BE-"
 palabra2 = "PN-"
 condicional = 0
 suma_opx = 0
+totalopx = 0
+mesopx = ""
 
-anio =  int(easygui.enterbox("Type the year"))
-str_anio = str(anio)
+
 
 
 ###########################################################
 #                   Archivo Output
 # #########################################################   
  
-# easygui.msgbox("Write a name for you output file")
-# archivos = str(easygui.enterbox(msg="Write a name to the output file "))  
-# archivo  = archivos + ".xlsx"
+archivos = str(easygui.enterbox(msg="Write a name to the output file "))  
+archivo  = archivos + ".xlsx"
  
 
-# easygui.msgbox("Enter the folder where you want the output file to be stored ")
-# directorio=str(easygui.diropenbox())
+easygui.msgbox("Enter the folder where you want the output file to be stored ")
+directorio=str(easygui.diropenbox())
 
 
  
-# ruta_completa = os.path.join(directorio, archivo)
-# print(ruta_completa)
+ruta_completa = os.path.join(directorio, archivo)
+print(ruta_completa)
  
-# # Verificar si el archivo existe
+# Verificar si el archivo existe
  
-# if not os.path.exists(ruta_completa):
-#     workbook = openpyxl.Workbook()      #Crear un nuevo libro
-#     hoja = workbook.active              #Crear una nueva hoja
-#     hoja.title = str_anio               #Título de la hoja = anio
-#     workbook.save(ruta_completa)        #Guardar el libro
+if not os.path.exists(ruta_completa):
+    workbook = openpyxl.Workbook()      #Crear un nuevo libro
+    hoja = workbook.active              #Crear una nueva hoja
+    hoja.title = str_anio               #Título de la hoja = anio
+    workbook.save(ruta_completa)        #Guardar el libro
  
-#     easygui.msgbox(f"The file {archivo} was created in: {directorio}")
+    easygui.msgbox(f"The file {archivo} was created in: {directorio}")
     
-# else:
-#     easygui.msgbox(f"The file {archivo} already exists in: {directorio}")
-#     workbook = openpyxl.load_workbook(ruta_completa)
-#     hoja = workbook.active              #Crear una nueva hoja
-#     hoja.title = str_anio               #Título de la hoja = anio
-#     workbook.save(ruta_completa)        #Guardar el libro
+else:
+    easygui.msgbox(f"The file {archivo} already exists in: {directorio}")
+    workbook = openpyxl.load_workbook(ruta_completa)
+    hoja = workbook.active              #Crear una nueva hoja
+    hoja.title = str_anio               #Título de la hoja = anio
+    workbook.save(ruta_completa)        #Guardar el libro
  
-# HOURS_PER_DAY =  easygui.enterbox("How many laboral hours does a day have? (8, 8.25)")
-HOURS_PER_DAY = 8.25
+HOURS_PER_DAY =  float(easygui.enterbox("How many laboral hours does a day have? (8, 8.25...)"))
+#HOURS_PER_DAY = 8.25
 
+num_fila = 1
+num_columna = 1
+
+
+
+#Imprimir encabezados
+#hoja.cell(row = num_fila)
 
 
 
@@ -119,17 +130,15 @@ HOURS_PER_DAY = 8.25
 
 for i in range(numhojas):
     df = excel_file.parse(hojas[i])
-    df_2= opx_file.parse(hojas_2[i])
+    df_2= opx_file.parse(hojas_2[0])
 
     
     easygui.msgbox("Answer the following box as follows, type 1 if you want to work in the month of January, 2 if in the month of February.....")
-    mes = int(easygui.enterbox("Type the month in which you want to work in opx in number(must be the same month as prime)"))    
+    mes = int(easygui.enterbox("Type the month in which you want to work in opx in number (must be the same month as prime)" + "This is the month of prime that you choose before:" + str(hojas[i])))    
 
+    num_proyectos = int(easygui.enterbox("How many projects do you want to work on this month " + str(hojas[i]) + "?"))    
 
-    #str easy gui enter box "Cuantos proyectos"
-    #num_proyectos = str(easygui.enterbox("How many project do you want to work"))    
-
-    num_proyectos = 1 #num the     
+    #num_proyectos = 1 #num the     
     df = df.dropna(subset=[nombrecolumna1, nombrecolumna2])
     column_headers = df_2.columns.tolist() #   
     df_2[column_headers] = df_2[column_headers].fillna(0)
@@ -170,13 +179,17 @@ for i in range(numhojas):
             #print(col_header)#verificar que si fuera correcta la comparacion
 
             for j in range(num_proyectos):    
-                proyecto = str(easygui.enterbox("Name of the project you want to work on? (Ford, Singer, tesla, brp, zoox)"))
-                #enterbox "For this project, how many areas"
-                num_areas = 1
+                proyecto = str(easygui.enterbox("Name of the project you want to work on? (Ford, Singer, Tesla, BRP, Zoox)"))
+                num_areas = int(easygui.enterbox("For this project: " + str(proyecto) + "; how many areas would you like to analyze?"))
+                #num_areas = 1
+                
                 for h in range (num_areas):
+                    if suma_prime !=0:
+                        suma_prime = 0
+                    if suma_opx !=0:
+                        suma_opx = 0
                     area = str(easygui.enterbox("Name of the area you want to work on this project (" + proyecto + ") (NET,CSW, ASW, etc)"))
-                    #ano = str(easygui.enterbox("Type the year you want to work on")) 
-
+                    
                     #print(df_2)
                     #print("Columnas de la hoja df_2:", column_headers)
                     #df_2[nom3] = df_2[nom3].fillna(0)
@@ -194,7 +207,10 @@ for i in range(numhojas):
 
                     for index, row in df_2.iterrows():                    
                         columna_1 = row[column_headers[0]]
-                        columna_2 = row[col_header]                        
+                        columna_2 = row[col_header] 
+                        if index == 0:
+                            #easygui.msgbox(columna_2)  
+                            mesopx = columna_2                     
                         
                         #print(columna_1)
                         #print(columna_2)
@@ -206,11 +222,15 @@ for i in range(numhojas):
                             condicional = 1
                         
                         if condicional == 1 and area in str(columna_1):
-                            suma_opx = suma_opx + columna_2              
+                            suma_opx = suma_opx + columna_2*HOURS_PER_DAY     
+
+                    easygui.msgbox("Mes Prime: " + str(hojas[i]) + "\nMes OPX: " + str(mesopx) + "\nProyecto: " + str(proyecto) + "\nArea: " + str(area)  + "\nHoras Prime: " + str(suma_prime) + "\nHojas OPX: " + str(suma_opx))
+                    # easygui.msgbox(suma_prime)
+                    # easygui.msgbox(suma_opx)        
 
                         
-print(suma_prime)
-print(suma_opx * HOURS_PER_DAY)
+
+
 
 
 
